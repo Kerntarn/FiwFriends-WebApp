8c981d1128581c91d82ca8d76cd8c3e0b17ba08d
nickytk <nccky.ades@gmail.com>
2025-02-11 13:34:37 +0700

    Update All CRUD User and Frontend Test User

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -64,10 +64,9 @@ public class UserController : Controller
         return View(user);
     }
 
-    // POST: Edit User
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(int id, [FromBody] User user)
+    public async Task<IActionResult> Edit(int id, User user)
     {
         if (id != user.UserId)
         {
@@ -78,9 +77,27 @@ public class UserController : Controller
         {
             try
             {
-                _db.Entry(user).State = EntityState.Modified;
+                var existingUser = await _db.Users.FirstOrDefaultAsync(u => u.UserId == id);
+                if (existingUser == null)
+                {
+                    return NotFound();
+                }
+
+                // Update only the properties that are provided in the form
+                existingUser.FirstName = user.FirstName;
+                existingUser.LastName = user.LastName;
+                existingUser.Username = user.Username;
+                existingUser.Bio = user.Bio;
+
+                // Only update the password if it's provided
+                if (!string.IsNullOrEmpty(user.Password))
+                {
+                    existingUser.Password = user.Password;
+                }
+
+                _db.Entry(existingUser).State = EntityState.Modified;
                 await _db.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return RedirectToAction("Index");
             }
             catch (DbUpdateConcurrencyException)
             {
@@ -94,9 +111,10 @@ public class UserController : Controller
                 }
             }
         }
+
+        // If we got this far, something failed; redisplay the form
         return View(user);
     }
-
     // GET: Delete User Confirmation
     public async Task<IActionResult> Delete(int id)
     {
