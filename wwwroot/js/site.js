854dd3bd04fb25549e5a2dadd4ae86a24ec27fdc
zJackDawz <koonainccc@gmail.com>
2025-02-17 17:29:42 +0700

    MakeRegisterPage
    เหมือน commit ที่แล้วแต่่เพิ่ม js เข้ามาให้พอเห็นภาพบ้างแล้ว

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function login() {
+        document.getElementById("login-text").textContent="incorrect username or password"
+}
+
+function signup() {
+        document.getElementById("register-text").textContent="incorrect username or password"
+}
