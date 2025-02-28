f9269704114585e496ef4c83bcddf81b8b20abc7
Nickytk <nccky.ades@gmail.com>
2025-03-01 01:06:16 +0700

    fix updateuserdtos.cs

--- DTOs/UpdateUserDto.cs
+++ DTOs/UpdateUserDto.cs
@@ -4,8 +4,11 @@ namespace FiwFriends.DTOs
 {
     public class UpdateUserDto
     {
+        [Required (ErrorMessage = "Username is required")]
         public required string Username { get; set; }
+        [Required (ErrorMessage = "FirstName is required")]
         public required string FirstName { get; set; }
+        [Required (ErrorMessage = "FirstName is required")]
         public required string LastName { get; set; }
         public string? Password { get; set; }
         public string? Bio {get; set;}
